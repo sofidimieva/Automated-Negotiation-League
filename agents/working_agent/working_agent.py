bde0be4a405af4a2eba4faf4437bf5bdcf8f196f
Polly Dimieva <p.dimieva@student.tudelft.nl>
2025-03-24 19:27:31 +0100

    Incorporated Nash product and Pareto frontier considerations

--- agents/working_agent/working_agent.py
+++ agents/working_agent/working_agent.py
@@ -27,8 +27,14 @@ from geniusweb.progress.ProgressTime import ProgressTime
 from geniusweb.references.Parameters import Parameters
 from tudelft_utilities_logging.ReportToLogger import ReportToLogger
 
+from .utils.opponent_model import OpponentModel
+
 
 class WorkingAgent(DefaultParty):
+    """
+    Template of a Python geniusweb agent.
+    """
+
     def __init__(self):
         super().__init__()
         self.logger: ReportToLogger = self.getReporter()
@@ -38,66 +44,223 @@ class WorkingAgent(DefaultParty):
         self.profile: LinearAdditiveUtilitySpace = None
         self.progress: ProgressTime = None
         self.me: PartyId = None
-        self.last_received_bid: Bid = None
+        self.other: str = None
+        self.settings: Settings = None
+        self.storage_dir: str = None
 
-        self.logger.log(logging.INFO, "WorkingAgent initialized")
+        self.last_received_bid: Bid = None
+        self.opponent_model: OpponentModel = None
+        self.logger.log(logging.INFO, "party is initialized")
 
     def notifyChange(self, data: Inform):
+        """MUST BE IMPLEMENTED
+        This is the entry point of all interaction with your agent after is has been initialised.
+        How to handle the received data is based on its class type.
+
+        Args:
+            info (Inform): Contains either a request for action or information.
+        """
+
+        # a Settings message is the first message that will be send to your
+        # agent containing all the information about the negotiation session.
         if isinstance(data, Settings):
-            self.progress = data.getProgress()
-            self.me = data.getID()
-            profile_conn = ProfileConnectionFactory.create(
+            self.settings = cast(Settings, data)
+            self.me = self.settings.getID()
+
+            # progress towards the deadline has to be tracked manually through the use of the Progress object
+            self.progress = self.settings.getProgress()
+
+            self.parameters = self.settings.getParameters()
+            self.storage_dir = self.parameters.get("storage_dir")
+
+            # the profile contains the preferences of the agent over the domain
+            profile_connection = ProfileConnectionFactory.create(
                 data.getProfile().getURI(), self.getReporter()
             )
-            self.profile = profile_conn.getProfile()
+            self.profile = profile_connection.getProfile()
             self.domain = self.profile.getDomain()
-            profile_conn.close()
+            profile_connection.close()
 
+        # ActionDone informs you of an action (an offer or an accept)
+        # that is performed by one of the agents (including yourself).
         elif isinstance(data, ActionDone):
             action = cast(ActionDone, data).getAction()
-            if action.getActor() != self.me and isinstance(action, Offer):
-                self.last_received_bid = cast(Offer, action).getBid()
+            actor = action.getActor()
 
+            # ignore action if it is our action
+            if actor != self.me:
+                # obtain the name of the opponent, cutting of the position ID.
+                self.other = str(actor).rsplit("_", 1)[0]
+
+                # process action done by opponent
+                self.opponent_action(action)
+        # YourTurn notifies you that it is your turn to act
         elif isinstance(data, YourTurn):
+            # execute a turn
             self.my_turn()
 
+        # Finished will be send if the negotiation has ended (through agreement or deadline)
         elif isinstance(data, Finished):
-            self.logger.log(logging.INFO, "Negotiation finished.")
+            self.save_data()
+            # terminate the agent MUST BE CALLED
+            self.logger.log(logging.INFO, "party is terminating:")
             super().terminate()
+        else:
+            self.logger.log(logging.WARNING, "Ignoring unknown info " + str(data))
 
     def getCapabilities(self) -> Capabilities:
+        """MUST BE IMPLEMENTED
+        Method to indicate to the protocol what the capabilities of this agent are.
+        Leave it as is for the ANL 2022 competition
+
+        Returns:
+            Capabilities: Capabilities representation class
+        """
         return Capabilities(
             set(["SAOP"]),
             set(["geniusweb.profile.utilityspace.LinearAdditive"]),
         )
 
     def send_action(self, action: Action):
+        """Sends an action to the opponent(s)
+
+        Args:
+            action (Action): action of this agent
+        """
         self.getConnection().send(action)
 
+    # give a description of your agent
     def getDescription(self) -> str:
-        return "Simple agent that offers high-utility bids and accepts near deadline."
+        """MUST BE IMPLEMENTED
+        Returns a description of your agent. 1 or 2 sentences.
+
+        Returns:
+            str: Agent description
+        """
+        return "Template agent for the ANL 2022 competition"
+
+    def opponent_action(self, action):
+        """Process an action that was received from the opponent.
+
+        Args:
+            action (Action): action of opponent
+        """
+        # if it is an offer, set the last received bid
+        if isinstance(action, Offer):
+            # create opponent model if it was not yet initialised
+            if self.opponent_model is None:
+                self.opponent_model = OpponentModel(self.domain)
+
+            bid = cast(Offer, action).getBid()
+
+            # update opponent model with bid
+            self.opponent_model.update(bid)
+            # set bid as last received
+            self.last_received_bid = bid
 
     def my_turn(self):
-        progress = self.progress.get(time() * 1000)
-        self.logger.log(logging.INFO, f"Progress: {progress:.2f}")
-
-        if self.last_received_bid:
-            utility = self.profile.getUtility(self.last_received_bid)
-            self.logger.log(logging.INFO, f"Received bid utility: {utility:.3f}")
-            if utility > 0.8 and progress > 0.9:
-                self.logger.log(logging.INFO, "Accepting bid.")
-                self.send_action(Accept(self.me, self.last_received_bid))
-                return
-
-        bid = self.find_high_utility_bid()
-        self.logger.log(logging.INFO, f"Offering bid with utility: {self.profile.getUtility(bid):.3f}")
-        self.send_action(Offer(self.me, bid))
-        
-    def find_high_utility_bid(self) -> Bid:
-        all_bids = AllBidsList(self.domain)
-        good_bids = [b for b in all_bids if self.profile.getUtility(b) >= 0.9]
-
-        if good_bids:
-            return good_bids[randint(0, len(good_bids) - 1)]
+        """This method is called when it is our turn. It should decide upon an action
+        to perform and send this action to the opponent.
+        """
+        # check if the last received offer is good enough
+        if self.accept_condition(self.last_received_bid):
+            # if so, accept the offer
+            action = Accept(self.me, self.last_received_bid)
         else:
-            return max(all_bids, key=lambda b: self.profile.getUtility(b))
+            # if not, find a bid to propose as counter offer
+            bid = self.find_bid()
+            action = Offer(self.me, bid)
+
+        # send the action
+        self.send_action(action)
+
+    def save_data(self):
+        """This method is called after the negotiation is finished. It can be used to store data
+        for learning capabilities. Note that no extensive calculations can be done within this method.
+        Taking too much time might result in your agent being killed, so use it for storage only.
+        """
+        data = "Data for learning (see README.md)"
+        with open(f"{self.storage_dir}/data.md", "w") as f:
+            f.write(data)
+
+    ###########################################################################################
+    ################################## Example methods below ##################################
+    ###########################################################################################
+
+    def accept_condition(self, bid: Bid) -> bool:
+        if bid is None:
+            return False
+
+        # progress of the negotiation session between 0 and 1 (1 is deadline)
+        progress = self.progress.get(time() * 1000)
+
+        # dynamically adjust acceptance threshold based on progress
+        threshold = 0.9 - 0.4 * progress
+
+        # accept if the offer is valued above the threshold
+        return self.profile.getUtility(bid) >= threshold
+
+    def find_bid(self) -> Bid:
+        # compose a list of all possible bids
+        domain = self.profile.getDomain()
+        all_bids = AllBidsList(domain)
+
+        best_bid_score = 0.0
+        best_bid = None
+
+        # take 500 attempts to find a bid according to a heuristic score
+        for _ in range(500):
+            bid = all_bids.get(randint(0, all_bids.size() - 1))
+            bid_score = self.score_bid(bid)
+            if bid_score > best_bid_score:
+                best_bid_score, best_bid = bid_score, bid
+
+        return best_bid
+
+    def score_bid(self, bid: Bid, alpha: float = 0.95, eps: float = 0.1) -> float:
+        """Calculate heuristic score for a bid
+
+        Args:
+            bid (Bid): Bid to score
+            alpha (float, optional): Trade-off factor between self interested and
+                altruistic behaviour. Defaults to 0.95.
+            eps (float, optional): Time pressure factor, balances between conceding
+                and Boulware behaviour over time. Defaults to 0.1.
+
+        Returns:
+            float: score
+        """
+        progress = self.progress.get(time() * 1000)
+
+        our_utility = float(self.profile.getUtility(bid))
+
+        time_pressure = 1.0 - progress ** (1 / eps)
+        score = alpha * time_pressure * our_utility
+
+        if self.opponent_model is not None:
+            opponent_utility = self.opponent_model.get_predicted_utility(bid)
+            opponent_score = (1.0 - alpha * time_pressure) * opponent_utility
+            score += opponent_score
+
+            # Incorporate Nash product and Pareto frontier considerations
+            nash_product = our_utility * opponent_utility
+            pareto_distance = self.calculate_pareto_distance(bid, our_utility, opponent_utility)
+            score += nash_product - pareto_distance
+
+        return score
+
+    def calculate_pareto_distance(self, bid: Bid, our_utility: float, opponent_utility: float) -> float:
+        """Calculate the distance of a bid from the Pareto frontier
+
+        Args:
+            bid (Bid): Bid to evaluate
+            our_utility (float): Our utility for the bid
+            opponent_utility (float): Opponent's utility for the bid
+
+        Returns:
+            float: Distance from the Pareto frontier
+        """
+        # For simplicity, we assume the Pareto frontier is the line where our utility + opponent's utility = 1
+        pareto_utility_sum = 1.0
+        actual_utility_sum = our_utility + opponent_utility
+        return abs(pareto_utility_sum - actual_utility_sum)
